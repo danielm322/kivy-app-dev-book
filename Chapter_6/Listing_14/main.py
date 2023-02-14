aa0d32aedbe019a0cf85f038ae1657066ebff9db
daniel <daniel.montoyav@gmail.com>
2023-02-14 10:59:21 +0100

    Chapter 6 files and listings (2)

--- Chapter_6/Listing_14/main.py
+++ Chapter_6/Listing_14/main.py
@@ -5,13 +5,22 @@ import kivy.app
 import kivy.uix.screenmanager
 import kivy.animation
 import kivy.clock
-
+import kivy.core.audio
+from os import getcwd
 
 class TestApp(kivy.app.App):
     def screen_on_pre_enter(self, screen_num):
         coin_width = 0.05
         coin_height = 0.05
         curr_screen = self.root.screens[screen_num]
+        curr_screen.character_killed = False
+        curr_screen.num_coins_collected = 0
+        curr_screen.ids['character_image_lvl' + str(screen_num)].im_num = 0
+        curr_screen.ids['monster_image_lvl' + str(screen_num)].im_num = 10
+        curr_screen.ids['num_coins_collected_lvl' + str(screen_num)].text = "Coins 0"
+        for key, coin in curr_screen.coins_ids.items():
+            curr_screen.ids['layout_lvl' + str(screen_num)].remove_widget(coin)
+        curr_screen.coins_ids = {}
         section_width = 1.0 / curr_screen.num_coins
         for k in range(curr_screen.num_coins):
             x = random.uniform(section_width * k, section_width * (k + 1) - coin_width)
@@ -23,6 +32,14 @@ class TestApp(kivy.app.App):
             curr_screen.coins_ids['coin' + str(k)] = coin
 
     def screen_on_enter(self, screen_num):
+        music_dir = getcwd() + "/music/"
+        self.bg_music = kivy.core.audio.SoundLoader.load(music_dir + "bg_music_piano.wav")
+        self.bg_music.loop = True
+        self.coin_sound = kivy.core.audio.SoundLoader.load(music_dir + "coin.wav")
+        self.level_completed_sound = kivy.core.audio.SoundLoader.load(music_dir + "level_completed_flaute.wav")
+        self.char_death_sound = kivy.core.audio.SoundLoader.load(music_dir + "char_death_flaute.wav")
+        self.bg_music.play()
+        
         curr_screen = self.root.screens[screen_num]
         monster_image = curr_screen.ids['monster_image_lvl' + str(screen_num)]
         new_pos = (random.uniform(0.0, 1 - monster_image.size_hint[0]),
@@ -30,6 +47,11 @@ class TestApp(kivy.app.App):
         self.start_monst_animation(monster_image=monster_image,
                                    new_pos=new_pos,
                                    anim_duration=random.uniform(1.5, 3.5))
+
+    def screen_on_pre_leave(self, screen_num):
+        curr_screen = self.root.screens[screen_num]
+        curr_screen.ids['monster_image_lvl' + str(screen_num)].pos_hint = {'x': 0.8, 'y': 0.8}
+        curr_screen.ids['character_image_lvl' + str(screen_num)].pos_hint = {'x': 0.2, 'y': 0.6}
 
     def start_monst_animation(self, monster_image, new_pos, anim_duration):
         monst_anim = kivy.animation.Animation(pos_hint={'x': new_pos[0], 'y': new_pos[1]},
@@ -60,6 +82,8 @@ class TestApp(kivy.app.App):
                 abs(character_center[0] - monster_center[0]) <= gab_x and \
                 abs(character_center[1] - monster_center[1]) <= gab_y and \
                 curr_screen.character_killed == False:
+            self.bg_music.stop()
+            self.char_death_sound.play()
             curr_screen.character_killed = True
             kivy.animation.Animation.cancel_all(character_image)
             kivy.animation.Animation.cancel_all(monster_image)
@@ -106,11 +130,14 @@ class TestApp(kivy.app.App):
             if character_image.collide_widget(curr_coin) and\
                     abs(character_center[0] - curr_coin_center[0]) <= gab_x and\
                     abs(character_center[1] - curr_coin_center[1]) <= gab_y:
+                self.coin_sound.play()
                 coins_to_delete.append(coin_key)
                 curr_screen.ids['layout_lvl' + str(screen_num)].remove_widget(curr_coin)
                 curr_screen.num_coins_collected = curr_screen.num_coins_collected + 1
                 curr_screen.ids['num_coins_collected_lvl' + str(screen_num)].text = "Coins " + str(curr_screen.num_coins_collected)
                 if curr_screen.num_coins_collected == curr_screen.num_coins:
+                    self.bg_music.stop()
+                    self.level_completed_sound.play()
                     kivy.animation.Animation.cancel_all(character_image)
                     kivy.clock.Clock.schedule_once(functools.partial(self.back_to_main_screen, curr_screen.parent), 3)
                     kivy.animation.Animation.cancel_all(curr_screen.ids['monster_image_lvl' + str(screen_num)])
